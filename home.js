b6565d1afc4b4f048853ec29e26f15ba3770b134
Marko Djukic <77008524+MarkoDjukic35@users.noreply.github.com>
2022-06-03 22:33:23 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,47 @@
+/*Menu*/
+var open = document.getElementById('hamburger');
+var changeIcon = true;
+
+open.addEventListener("click", function() {
+
+    var overlay = document.querySelector('.overlay');
+    var nav = document.querySelector('nav');
+    var icon = document.querySelector('.menu-toggle i');
+
+    overlay.classList.toggle("menu-open");
+    nav.classList.toggle("menu-open");
+
+    if (changeIcon) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+
+        changeIcon = false;
+    } else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+        changeIcon = true;
+    }
+});
+
+// Close Menu when click on Link
+
+$('.menu li a').on("click", function() {
+    $('#hamburger').click();
+});
+
+
+
+/*Menu*/
+
+/*Hero section*/
+
+/*Hero section*/
+
+
+/*Skills section*/
+
+/*Skills section*/
+
+/*Kontakt section*/
+
+/*Kontakt section*/
