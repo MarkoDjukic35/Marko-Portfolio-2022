3416513b1b830b664a24b93f022d8c51dad8aab9
Marko Djukic <77008524+MarkoDjukic35@users.noreply.github.com>
2022-06-04 19:25:10 +0200

    novo

--- home.js
+++ home.js
@@ -1,35 +1,18 @@
 /*Menu*/
-var open = document.getElementById('hamburger');
-var changeIcon = true;
-
-open.addEventListener("click", function() {
-
-    var overlay = document.querySelector('.overlay');
-    var nav = document.querySelector('nav');
-    var icon = document.querySelector('.menu-toggle i');
-
-    overlay.classList.toggle("menu-open");
-    nav.classList.toggle("menu-open");
-
-    if (changeIcon) {
-        icon.classList.remove("fa-bars");
-        icon.classList.add("fa-times");
-
-        changeIcon = false;
-    } else {
-        icon.classList.remove("fa-times");
-        icon.classList.add("fa-bars");
-        changeIcon = true;
-    }
-});
 
 // Close Menu when click on Link
 
 
 
 
-
-/*Menu*/
+function myFunction() {
+    var x = document.getElementById("centered_nav");
+    if (x.className === "rc_nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "rc_nav";
+    }
+}
 
 /*Hero section*/
 
